19b94f444b318edc28c16e2ffff5972cceab54dc
Severin Friede <sevefriede@gmail.com>
2018-01-19 12:06:44 +0100

    fixed row height computation

--- playground/index.ts
+++ playground/index.ts
@@ -7,18 +7,29 @@ import { Component, OnInit } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { ScrollViewportModule } from '../dist';
+import { ViewChild } from '@angular/core/src/metadata/di';
 
 @Component({
   selector: 'app',
   template: `
     <div>
       <h2>Angular Scroll Viewport</h2>
+      <input type="text" #change value="100000">
+      <button (click)="setRowCount(change.value)">Set Rows</button><br/><br/>
+      <input type="text" #overscan value="10">
+      <button (click)="setOverscan(overscan.value)">Set Overscan</button><br/><br/>
+      <button (click)="toggle = !toggle">Toggle View (Current: {{ (!toggle ? 'scroll-viewport' : 'ngFor') }})</button>
       <p>Showing {{rowCount}} rows</p>
-      <scroll-viewport [items]="items" [overscan]="20" [rowHeight]="30">
-        <ng-template let-item>
-          <div class="row">{{item}}</div>
-        </ng-template>
-      </scroll-viewport>
+      <div *ngIf="!toggle" style="height: 600px; overflow: auto; border: 1px solid black;">
+        <scroll-viewport [items]="items" [overscan]="overscanValue">
+          <ng-template let-item>
+            <div class="row">{{item}}</div>
+          </ng-template>
+        </scroll-viewport>
+      </div>
+      <div *ngIf="toggle" style="height: 600px; overflow: auto; border: 1px solid black;">
+        <div *ngFor="let item of items" class="row">{{item}}</div>
+      </div>
     </div>
   `,
   styles: [`
@@ -37,10 +48,21 @@ import { ScrollViewportModule } from '../dist';
 class AppComponent implements OnInit {
   items: any[] = [];
   rowCount: string = '';
+  toggle = false;
+  overscanValue = 10;
 
   ngOnInit() {
+    this.setRowCount('100000');
+  }
+
+  setOverscan(value: string) {
+    this.overscanValue = +value;
+  }
+
+  setRowCount(value: string) {
+    const count = +value;
     const data = [];
-    for (let x=1e5; x--; ) {
+    for (let x=count; x--; ) {
       data[x] = `Item #${x+1}`;
     };
 
--- src/scroll-viewport.component.ts
+++ src/scroll-viewport.component.ts
@@ -1,18 +1,24 @@
-import { Component, ElementRef, OnInit, Input, OnDestroy, Inject, ChangeDetectorRef, ContentChild, TemplateRef, HostBinding, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Input,
+  OnDestroy,
+  Inject,
+  ChangeDetectorRef,
+  ContentChild,
+  TemplateRef,
+  HostBinding,
+  OnChanges,
+  AfterViewInit } from '@angular/core';
 import { NgForOfContext } from '@angular/common';
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 
-export interface ScrollViewportState {
-  top: string;
-  height: number;
-  visibleItems: any[];
-}
-
 const EVENT_OPTS = {
-	passive: true,
-	capture: true
+  passive: true,
+  capture: true
 };
 
 @Component({
@@ -20,30 +26,27 @@ const EVENT_OPTS = {
   templateUrl: './scroll-viewport.component.html',
   styleUrls: ['./scroll-viewport.component.css'],
 })
-export class ScrollViewportComponent implements AfterViewInit, OnDestroy {
-  
+export class ScrollViewportComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
+
   @Input() items: any[] = [];
   @Input() rowHeight: number;
   @Input() defaultRowHeight: number;
-  @Input() overscan: number = 10;
-  @Input() sync: boolean = false;
+  @Input() overscan = 10;
 
   @ContentChild(TemplateRef) itemTemplate: TemplateRef<NgForOfContext<any>>;
 
   @HostBinding('style.height') hostHeight: string;
 
-  state: ScrollViewportState = {
-    top: '0px',
-    height: 0,
-    visibleItems: []
-  };
+  top = '0px';
+  visibleItems: any[];
 
   private _height: number;
-  private offset: number = 0;
+  private height: number;
+  private offset = 0;
 
   private resizeSub: Subscription;
   private scrollSub: Subscription;
-  
+
   constructor(
     private elementRef: ElementRef,
     private changeDetectorRef: ChangeDetectorRef,
@@ -51,13 +54,13 @@ export class ScrollViewportComponent implements AfterViewInit, OnDestroy {
     @Inject('DOCUMENT') private documentRef: any,
   ) {}
 
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit');
-    this.resized();
-    console.log('ngAfterViewInit 2 ');    
-    this.scrolled();
-    console.log('ngAfterViewInit 3');
+  ngOnInit() {
+    // we have to render at leat one element to properly compute the row height
+    if (!this.visibleItems) {
+      this.visibleItems = this.items.slice(0, 1);
+    }
 
+    // subscribe to resize and scroll events
     this.resizeSub = Observable.fromEvent(this.windowRef, 'resize', EVENT_OPTS)
       .subscribe(event => {
         this.resized();
@@ -67,7 +70,24 @@ export class ScrollViewportComponent implements AfterViewInit, OnDestroy {
         this.scrolled();
       });
   }
-  
+
+  ngOnChanges() {
+    if (this.visibleItems) {
+      console.log('ngOnChanges');
+      this.update({ height: this.height, offset: this.offset });
+    }
+  }
+
+  ngAfterViewInit() {
+    // initially at least one element has been rendered
+    // so we trigger another change detection with the computed height
+    setTimeout(() => {
+      this.resized();
+      this.scrolled();
+      this.changeDetectorRef.markForCheck();
+    });
+  }
+
   ngOnDestroy() {
     if (this.resizeSub) {
       this.resizeSub.unsubscribe();
@@ -78,15 +98,15 @@ export class ScrollViewportComponent implements AfterViewInit, OnDestroy {
   }
 
   private update(changes: { height: number; offset: number; }) {
-    this.rowHeight = this.rowHeight || this.computeRowHeight() || this.defaultRowHeight || 100;
-    const estimatedHeight = this.rowHeight * this.items.length;
-  
+    const rowHeight = this.rowHeight || this.computeRowHeight() || this.defaultRowHeight || 100;
+    const estimatedHeight = rowHeight * this.items.length;
+
     let start = 0;
     let visibleRowCount = 1;
-    
-    if (this.rowHeight) {
-      start = (changes.offset / this.rowHeight) | 0;
-      visibleRowCount = (changes.height / this.rowHeight) | 0;
+
+    if (rowHeight) {
+      start = (changes.offset / rowHeight) | 0;
+      visibleRowCount = (changes.height / rowHeight) | 0;
 
       if (this.overscan) {
         start = Math.max(0, start - (start % this.overscan));
@@ -95,20 +115,17 @@ export class ScrollViewportComponent implements AfterViewInit, OnDestroy {
     }
 
     const end = start + 1 + visibleRowCount;
-    const visibleItems = this.items.slice(start, end);
-    const top = start * this.rowHeight;
+    const top = start * rowHeight;
+    this.height = changes.height;
 
+    this.visibleItems = this.items.slice(start, end);
+    this.top = top + 'px';
     this.hostHeight = estimatedHeight + 'px';
-    this.state = {
-      top: top + 'px',
-      height: changes.height,
-      visibleItems
-    };
   }
 
   private resized() {
     const height = this.windowRef.innerHeight || this.documentRef.documentElement.offsetHeight;
-    if (height !== this.state.height) {
+    if (height !== this.height) {
       this.update({ height, offset: this.offset });
     }
   }
@@ -116,10 +133,7 @@ export class ScrollViewportComponent implements AfterViewInit, OnDestroy {
   private scrolled() {
     const base = this.elementRef.nativeElement;
     const offset = Math.max(0, base && -base.getBoundingClientRect().top || 0);
-    this.update({ offset, height: this.state.height });
-    if (this.sync) {
-      this.changeDetectorRef.detectChanges();
-    }
+    this.update({ offset, height: this.height });
   }
 
   private computeRowHeight() {
@@ -128,9 +142,7 @@ export class ScrollViewportComponent implements AfterViewInit, OnDestroy {
     }
     const base = this.elementRef.nativeElement;
     const first = base && base.firstElementChild && base.firstElementChild.firstElementChild;
-    // TODO: cross browser way for offsetHeight
     this._height = (first && first.offsetHeight || 0);
-    console.log('_height: ' + this._height);
     return this._height;
   }
 }
